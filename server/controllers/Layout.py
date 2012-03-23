d9fa8d23c4ab57391f572391d5f6fa3b216a548d
matthias bock <matthias.bock@hu-berlin.de>
2012-03-23 13:14:58 +0100

    asynchronous server calls

--- server/controllers/Layout.py
+++ server/controllers/Layout.py
@@ -1,11 +1,11 @@
 # -*- coding: utf-8 -*-
 
 def biographer():
-	graph = Graph()
-	graph.importJSON( request.vars.JSON.read() )
-	layout( graph, path_to_layout_binary=os.path.join(request.folder, "layout/build/layout
-"), execution_folder=os.path.join(request.folder, "cache") )
+"""	graph = Graph()
+	graph.importJSON( request.vars.JSON )
+	layout( graph, path_to_layout_binary=os.path.join(request.folder, "layout/build/layout"), execution_folder=os.path.join(request.folder, "cache") )
 	graph.import_from_Layouter( layout )
 
 	return graph.exportJSON()
-
+"""
+	return request.vars.JSON
--- server/static/tools/httplib.js
+++ server/static/tools/httplib.js
@@ -1,15 +1,36 @@
-function GET(URL) {
+function GET(URL, handler) {
 	var r = new XMLHttpRequest();
-	r.open("GET", URL, false);
-	r.send(null);
-	return r.responseText;
+	if (handler != null) {
+		r.open("GET", URL, true);
+		r.onreadystatechange = function() {
+					if (this.readyState == this.DONE)
+						handler(this.responseText);
+					}
+		r.send(form);
+		}
+	else	{
+		r.open("GET", URL, false);
+		r.send(null);
+		return r.responseText;
+		}
 	}
 
-function POST(URL, form) {
+function POST(URL, form, handler) {
 	var r = new XMLHttpRequest();
-	r.open("POST", URL, false);
-	r.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
-	r.send(form);
-	return r.responseText;
+	if (handler != null) {
+		r.open("POST", URL, true);
+		r.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+		r.onreadystatechange = function() {
+					if (this.readyState == this.DONE)
+						handler(this.responseText);
+					}
+		r.send(form);
+		}
+	else	{
+		r.open("POST", URL, false);
+		r.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+		r.send(form);
+		return r.responseText;
+		}
 	}
 
