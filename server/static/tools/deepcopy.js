195aba5d2ca9484e5e71c58288ba39d1d96fe424
matthias bock <matthias.bock@hu-berlin.de>
2012-04-17 18:00:01 +0200

    autocommit

--- server/static/tools/deepcopy.js
+++ server/static/tools/deepcopy.js
@@ -1,3 +1,12 @@
 function deepCopy(obj) {
-	return new obj;
+	var copy = {};
+	for (key in obj)
+		copy[key] = obj[key];
+
+	if (typeof(obj) === '[object Object]') {
+		return copy;
+		}
+	if (typeof(obj) === '[object Array]') {
+		return Object.values(copy);
+		}
 	}
