8d15be8665d281ef678c9e72cd68259f49ac2528
matthias bock <matthias.bock@hu-berlin.de>
2012-04-17 08:09:31 +0200

    import Annotations works

--- server/controllers/Get.py
+++ server/controllers/Get.py
@@ -5,5 +5,9 @@
 
 # deactivated for now, since the database-download functions shall be moved to the client
 
-def Whi2p():
+def Whi2p_boolenet():
 	return open('/home/code/biosimulator/demo/Whi2p.boolenet').read()
+
+def Whi2p_annotations():
+	return open('/home/code/biosimulator/demo/Whi2p.annotations').read()
+
