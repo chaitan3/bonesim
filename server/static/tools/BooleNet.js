df37455d6f166d6be336234083720cc047c96238
matthias bock <matthias.bock@hu-berlin.de>
2012-04-24 08:35:37 +0200

    simulator bugfix

--- server/static/tools/BooleNet.js
+++ server/static/tools/BooleNet.js
@@ -135,7 +135,7 @@ BooleNet = {
 								var annotation = setup.substring(a, b);
 								if (typeof(node.simulation) === 'undefined')
 									node.simulation = {};
-								node.simulation.annotation = annotation;
+								node.simulation.annotation = annotation.replace('<iframe src=', '<iframe width="99%" height="85%" src=');
 //								console.log(node.id+' annotation: '+a+'-'+b+'-'+annotation);
 								}
 							}
