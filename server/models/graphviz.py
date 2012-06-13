10fc5fa40b668bb445e4ebf8e892583f5bf5a56f
matthias bock <matthias.bock@hu-berlin.de>
2012-06-13 22:26:35 +0200

    Chromium bugfix

--- server/models/graphviz.py
+++ server/models/graphviz.py
@@ -20,36 +20,34 @@ def WorkaroundChromiumIssue123607(line):
 	def Round(n):
 		n = n.group(0)
 		if n.find('.') > -1:
-			return n[:n.find('.')]
-		if n.find(',') > -1:
-			return n[:n.find('.')]
+			n = n[:n.find('.')]
 		return n
 	import re
-	points = re.sub('[0-9]*[\.,][0-9]*', Round, points)
+	points = re.sub('[0-9]*[\.\,][0-9]*', Round, points)
 	line = line[:p]+points+line[q:]
 	return line
 
 def prepareSVG(f):
-	infile = open(f).read().split('\n')
+	infile = open(f).read().replace('><', '>\n<').split('\n')
 	outfile = open(f, 'w')
 
 	inside_node = False
 	new_g = ''
 	for line in infile:
-		if '<polygon ' in line:
+		if 'points="' in line:
 			line = WorkaroundChromiumIssue123607(line)
 		if '"graph1"' in line:		# required for SVGPan.js
 			line = line.replace('"graph1"', '"viewport"').replace('scale(1 1)', 'scale(0.7 0.7)')
 		if inside_node:
-			if not '<title>' in line:	# we don't need the <title>
-				new_g += line+'\n'
+			if line.find('<title>') < 0:	# we don't need the <title>
+				new_g += line.replace('fill="none"', 'fill="white"')+'\n'
 			if '</g>' in line:		# node ends
 				if '<text ' in new_g:	# not a process node, which don't have a label
 					p = new_g.find('>', new_g.find('<text '))+1		# set ellipse id according to text label
 					q = new_g.find('</text>', p)
 					label = new_g[p:q]
 					name = label.replace(' ','_')
-					new_g = new_g.replace('fill="none"', 'id="'+name+'" fill="white" style="cursor:pointer"')
+					new_g = new_g.replace('fill="white"', 'id="'+name+'" fill="white" style="cursor:pointer"')
 				outfile.write(new_g)
 				inside_node = False
 		else:
